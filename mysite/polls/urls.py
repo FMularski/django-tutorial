5358dc81a6d988558a7ab270e655705c5e574414
Filip Mularski <mularskif@gmail.com>
2021-02-23 19:33:35 +0100

    views with parameter

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
-
+from . import views
 
 urlpatterns = [
-    path('', index, name='polls_index')
-]
+    path('', views.index, name='polls_index'),                                   # polls/
+    path('<int:question_id>/', views.detail, name='question_detail'),            # polls/id
+    path('<int:question_id>/detail', views.results, name='question_results'),    # polls/id/details
+    path('<int:question_id>/vote', views.vote, name='question_vote'),             # polls/id/vote
+    path('<int:question_id>/show', views.get_question, name='get_question')
+]  
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-import json
-
 
 def index(request):
-   return HttpResponse('Hello')
+   return HttpResponse('Index of polls.')
+
+def detail(request, question_id):
+   return HttpResponse(f'You are looking at question {question_id}')
+
+def results(request, question_id):
+   response = f'You are looking at question {question_id}'
+   return HttpResponse(response)
+
+def vote(request, question_id):
+   return HttpResponse(f'You are voting on question {question_id}')
+
+def get_question(request, question_id):
+   question = Question.objects.get(id=question_id)
+   return HttpResponse(question)
