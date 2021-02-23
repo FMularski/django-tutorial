5b0acde7abc79d772fffa478401369e32d79b63c
Filip Mularski <mularskif@gmail.com>
2021-02-23 23:36:15 +0100

    no hardcoded urls in templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('questions/<int:question_id>', views.question, name='question'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Question, Choice
 
@@ -7,7 +7,7 @@ def index(request):
    return render(request, 'polls/index.html', {'questions': questions})
 
 def question(request, question_id):
-   question = Question.objects.get(id=question_id)
+   question = get_object_or_404(Question, id=question_id)
    choices = question.choice_set.all()
    return render(request, 'polls/question.html', {'question': question, 'choices': choices})
 
