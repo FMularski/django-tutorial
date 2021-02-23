f153d09662784df801c8980bc1328f4b58607275
Filip Mularski <mularskif@gmail.com>
2021-02-23 13:04:24 +0100

    registered Question and Choise to admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
     text = models.CharField(max_length=255, null=False)
     pub_date = models.DateTimeField('date published')   # human-readable name
 
+    def __str__(self):
+        return self.text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=255, null=False)
     votes = models.IntegerField(default=0, null=False)
+
+    def __str__(self):
+        return self.text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+import json
+
 
 def index(request):
-    return HttpResponse('Hello world!')
+   return HttpResponse('Hello')
